49bd89374911f78b659c483637a0a6e042664353
Kris Luo <krisldandan@gmail.com>
2017-09-13 18:06:03 -0400

    set up

---
+++ Minesweerper/Minesweerper/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Minesweerper
+//
+//  Created by Dan Luo on 2017/9/7.
+//  Copyright © 2017年 Dan Luo. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
---
+++ Minesweerper/Pods/Target Support Files/Pods-Minesweerper/Pods-Minesweerper-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Minesweerper : NSObject
+@end
+@implementation PodsDummy_Pods_Minesweerper
+@end
