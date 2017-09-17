2898b8eeb3b2cdd1a5e939c9af075dd2a8f865a7
Wenya Zhu <juliewenya@gmail.com>
2017-09-16 22:40:27 -0400

    TextField reflection

--- Minesweerper/Minesweerper/RegistrationViewController.swift
+++ Minesweerper/Minesweerper/RegistrationViewController.swift
@@ -14,7 +14,7 @@ import FirebaseAuth
 
 
 
-class RegistrationViewController: UIViewController {
+class RegistrationViewController: UIViewController, UITextFieldDelegate {
 
     var email : String = ""
     var userName : String = ""
@@ -31,17 +31,25 @@ class RegistrationViewController: UIViewController {
     @IBOutlet weak var passwordMessage: UILabel!
     
     @IBAction func RegistButton(_ sender: UIButton) {
-        email = emailRegistor.text!
-        userName = userNameRegistor.text!
-        passWord = passwordRegistor.text!
         
-        func isValidEmail(testStr:String) -> Bool {
-            // print("validate calendar: \(testStr)")
-            let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
-            
-            let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
-            return emailTest.evaluate(with: testStr)
-        }
+        
+    }
+    
+    override func viewDidLoad() {
+        emailRegistor.addTarget(self, action: #selector(emailFieldDidChange(textField:)), for: UIControlEvents.editingDidEnd)
+        userNameRegistor.addTarget(self, action: #selector(userNameFieldDidChange(textField:)), for:  UIControlEvents.editingChanged)
+        passwordRegistor.addTarget(self, action: #selector(passwordFieldDidChange(textField:)), for:  UIControlEvents.editingChanged)
+    }
+    
+    func isValidEmail(testStr:String) -> Bool {
+        // print("validate calendar: \(testStr)")
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: testStr)
+    }
+    
+    func emailFieldDidChange(textField: UITextField){
         
         //Check if email is empty
         if email == ""{
@@ -49,16 +57,25 @@ class RegistrationViewController: UIViewController {
             emailMessage.text = "Email should not be empty!"
         }
         
+        //Check if email is valid
+        if !isValidEmail(testStr: email){
+            emailMessage.textColor = UIColor.red
+            emailMessage.text = "Please input a valid email"
+        }else{
+            
+            emailMessage.text = ""
+        }
+       
+
+    }
+    
+    //
+    func userNameFieldDidChange(textField: UITextField){
+        
         //Check if user name is empty
         if userName == ""{
             userNameMessage.textColor = UIColor.red
             userNameMessage.text = "UserName should not be empty!"
-        }
-        
-        //Check if passWord is empty
-        if passWord == "" {
-            passwordMessage.textColor = UIColor.red
-            passwordMessage.text = "Password should not be empty!"
         }
         
         //Check length of user name
@@ -70,6 +87,19 @@ class RegistrationViewController: UIViewController {
         if userName.characters.count > 15{
             userNameMessage.textColor = UIColor.red
             userNameMessage.text = "User name should be no longer than 15 characters"
+        }
+
+        if userName.characters.count >= 4 && userName.characters.count <= 15{
+            userNameMessage.text = ""
+        }
+    }
+    
+    func passwordFieldDidChange(textField: UITextField){
+        
+        //Check if passWord is empty
+        if passWord == "" {
+            passwordMessage.textColor = UIColor.red
+            passwordMessage.text = "Password should not be empty!"
         }
         
         //Check length of passWord
@@ -83,25 +113,23 @@ class RegistrationViewController: UIViewController {
             passwordMessage.text = "Password should be no longer than 16 characters"
         }
         
-        //Check if email is valid
-        if !isValidEmail(testStr: email){
-            emailMessage.textColor = UIColor.red
-            emailMessage.text = "Please input a valid email"
-        }
-        
-        if userName.characters.count >= 4 && userName.characters.count <= 15{
-            
-        }
-        
         if passWord.characters.count >= 6 && passWord.characters.count <= 16 {
-            
+            passwordMessage.text = ""
         }
-        
-        if isValidEmail(testStr: email){
-            
-        }
-        
         
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
