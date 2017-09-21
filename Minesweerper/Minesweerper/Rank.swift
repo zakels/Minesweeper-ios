a506b1088462bda11913d19e0468a0cd0d74bde2
Wenya Zhu <wenyazhu@pal-nat186-31-236.itap.purdue.edu>
2017-09-20 21:01:14 -0400

    Rank

---
+++ Minesweerper/Minesweerper/Rank.swift
@@ -0,0 +1,13 @@
+//
+//  Rank.swift
+//  Minesweerper
+//
+//  Created by Wenya Zhu on 9/20/17.
+//  Copyright © 2017 Dan Luo. All rights reserved.
+//
+
+import Foundation
+
+public class Rank {
+
+}
