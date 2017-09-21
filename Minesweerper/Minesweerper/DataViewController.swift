7ef084a4d8fa61a75171a4c659de64e7d29ffa60
Kris Luo <krisldandan@gmail.com>
2017-09-20 22:02:45 -0400

    login finish

--- Minesweerper/Minesweerper/DataViewController.swift
+++ Minesweerper/Minesweerper/DataViewController.swift
@@ -7,11 +7,34 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class DataViewController: UIViewController {
 
     @IBOutlet weak var dataLabel: UILabel!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     var dataObject: String = ""
+
+    @IBAction func loginToMain(_ sender: UIButton) {
+        let email = usernameField.text
+        let password = passwordField.text
+        
+        Auth.auth().signIn(withEmail: email!, password: password!) { (user, error) in
+            if error == nil {
+                self.performSegue(withIdentifier: "LoginSegue", sender: self)
+            } else {
+                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                alert.addAction(UIAlertAction.init(title: "OK", style: .default, handler: { (action) in
+                    alert.dismiss(animated: true, completion: nil)
+                }))
+                self.present(alert, animated: true, completion: nil);
+            }
+        }
+    }
+    
 
 
     override func viewDidLoad() {
@@ -26,7 +49,7 @@ class DataViewController: UIViewController {
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.dataLabel!.text = dataObject
+        self.dataLabel.text = dataObject
     }
 
 
