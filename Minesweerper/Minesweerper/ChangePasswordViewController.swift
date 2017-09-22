ceaac408c5c60b3dd2a5e10f0156be1e746f5f01
Kris Luo <krisldandan@gmail.com>
2017-09-21 22:37:14 -0400

    Change Password

--- Minesweerper/Minesweerper/ChangePasswordViewController.swift
+++ Minesweerper/Minesweerper/ChangePasswordViewController.swift
@@ -10,6 +10,7 @@ import Foundation
 import UIKit
 import FirebaseDatabase
 import FirebaseAuth
+import Firebase
 
 class ChangePasswordViewController: UIViewController {
     
@@ -25,9 +26,77 @@ class ChangePasswordViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         self.uid = (Auth.auth().currentUser?.uid)!
+        let ref = Database.database().reference().child("Users").child(self.uid)
+        ref.observeSingleEvent(of: .value, with: { (snapshot) in
+            let dictionary = snapshot.value as? [String: AnyObject]
+            self.cuser = Users.init(uid: self.uid, dictionary: dictionary!)
+        })
     }
     
     @IBAction func submitChange(_ sender: UIButton) {
+        var valid: Bool = true
+        if !passwordCheck() {
+            valid = false
+        }
+        if !repeatCheck() {
+            valid = false
+        }
         
+        if valid == true {
+            let credencial = EmailAuthProvider.credential(withEmail: (cuser?.email)!, password: (cuser?.password)!)
+            let user = Auth.auth().currentUser
+            user?.reauthenticate(with: credencial, completion: { (error) in
+                if error == nil {
+                    self.cuser?.password = self.repeatPassword.text!
+                    user?.updatePassword(to: (self.cuser?.password)!)
+                    let ref = Database.database().reference().child("Users").child(self.uid)
+                    ref.updateChildValues((self.cuser?.toTable())!)
+                    self.performSegue(withIdentifier: "ChangePasswordSegue", sender: self)
+                }
+                
+            })
+        }
+    }
+    
+    func passwordCheck() -> Bool {
+        
+        var result : Bool = false
+        //Check if passWord is empty
+        if newPassword.text == "" {
+            passwordMessage.textColor = UIColor.red
+            passwordMessage.text = "Password should not be empty!"
+        }
+            
+            //Check length of passWord
+        else if (newPassword.text?.characters.count)! < 6{
+            passwordMessage.textColor = UIColor.red
+            passwordMessage.text = "Password should be at least 6 characters"
+        }
+            
+        else if (newPassword.text?.characters.count)! > 16{
+            passwordMessage.textColor = UIColor.red
+            passwordMessage.text = "Password should be no longer than 16 characters"
+        }
+            
+        else {
+            passwordMessage.text = ""
+            result = true
+        }
+        
+        return result
+        
+    }
+    
+    func repeatCheck() -> Bool {
+        if newPassword.text == repeatPassword.text {
+            repeatMessage.text = ""
+            return true
+        }
+            
+        else{
+            repeatMessage.textColor = UIColor.red
+            repeatMessage.text = "Not match with password"
+            return false
+        }
     }
 }
