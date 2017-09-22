9ad4d2f04efb01a914742eeeaf581331a6b93e6b
Wenya Zhu <juliewenya@gmail.com>
2017-09-21 23:11:41 -0400

    Rank

--- Minesweerper/Minesweerper/Rank.swift
+++ Minesweerper/Minesweerper/Rank.swift
@@ -5,47 +5,44 @@
 //  Created by Wenya Zhu on 9/20/17.
 //  Copyright © 2017 Dan Luo. All rights reserved.
 //
-
 import Foundation
 import Firebase
 import FirebaseDatabase
 import FirebaseAuth
 
-public class Record {
-    var uid : String = ""
-    var points : Int = 0
-    var userName : String = ""
+public class Rank {
+    
     var level : Int = 0
     
-    init(level: Int, points: Int) {
-        self.uid = (Auth.auth().currentUser?.uid)!
-        let ref = Database.database().reference().child("Users").child(uid)
-        ref.observeSingleEvent(of: .value, with: { (snapshot) in
+    init(level: Int){
+        self.level = level
+    }
+    
+    func getTopRanks() -> [String]{
+        let str = "level" + String(level)
+        var strs : [String] = []
+        var ref : DatabaseReference!
+        ref = Database.database().reference().child("Ranks").child(str)
+        let query = ref.queryOrdered(byChild: "points").queryLimited(toLast: 5)
+        
+        query.observeSingleEvent(of: .value, with: { (snapshot) in
             let dictionary = snapshot.value as? [String: AnyObject]
-            self.userName = (dictionary?["username"] as? String)!
+            print(dictionary ?? "DICTIONARY ERROR")
+            let uid = dictionary?["uid"]
+            var userName : String = ""
+            let userRef = Database.database().reference().child("Users").child(uid as! String)
+            userRef.observeSingleEvent(of: .value, with: {(snap) in
+                let dict = snap.value as? [String: AnyObject]
+                userName = (dict?["username"])! as! String
+            })
             
+            let points = dictionary?["points"]
+            strs.append(userName + String(describing: points))
         }) { (error) in
             print(error.localizedDescription)
         }
         
-    }
-    
-    func creatRecord(record: Record){
-        var size : Int = 0
-        
-        let level = "Level" + String(record.level)
-        let ref = Database.database().reference().child("Ranks").child(level)
-        ref.observe(.value, with: { snapshot in
-            for _ in snapshot.children{
-                size += 1
-            }
-        })
-        
-        let rankStr : String = "Rank" + String(size + 1)
-        let recordRef = ref.child(rankStr)
-        recordRef.setValue(["uid": record.uid])
-        recordRef.setValue(["points": record.points])
-        
+        return strs
     }
     
 }
